7822ca880fc5839d77b824164581610777296fbb
Vitalii Dubovyk <58308423+vetal54@users.noreply.github.com>
2020-12-29 16:39:18 +0200

    Laba_5_2

---
+++ Figure.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace LR5Task2Forms
+{
+    abstract class Figure
+    {
+        public Color colour
+        {
+            get;
+            set;
+        }
+        private double X_;
+        private double Y_;
+        public double x
+        {
+            get
+            {
+                return X_;
+            }
+            set
+            {
+                X_ = x;
+            }
+        }
+        public double y
+        {
+            get
+            {
+                return Y_;
+            }
+            set
+            {
+                Y_ = y;
+            }
+        }
+        public abstract void DrawBlack();
+        public abstract void HideDrawingBackGround();
+        public void MoveRight()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                DrawBlack();
+                System.Threading.Thread.Sleep(50);
+                HideDrawingBackGround();
+                X_++;
+            }
+        }
+
+
+
+    }
+}
---
+++ Rhomb.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace LR5Task2Forms
+{
+    class Rhomb:Figure
+    {
+        protected Form1 form1;
+        protected double horDiagLen;
+        protected double verDiagLen;
+        public Rhomb(double x, double y, double horDiagLen, double verDiagLen, Form1 form1)
+        {
+
+            this.form1 = form1;
+            this.horDiagLen = horDiagLen;
+            this.verDiagLen = verDiagLen;
+            this.x = x;
+            this.y = y;
+        }
+        public override void DrawBlack()
+        {
+            using (GraphicsPath myPath = new GraphicsPath())
+            {
+                myPath.AddLines(new[]
+                {
+                    new Point((int)this.x,  (int)this.y + ((int)this.verDiagLen  / 2)),
+                    new Point((int)this.x + ((int)this.horDiagLen/ 2), (int)this.y),
+                    new Point((int)this.x + (int)this.horDiagLen, (int)this.y + ((int)this.verDiagLen / 2)),
+                    new Point((int)this.x + ((int)this.horDiagLen / 2), (int)this.y + (int)this.verDiagLen ),
+                    new Point((int)this.x, (int)this.y + ((int)this.verDiagLen  / 2))
+                });
+                using (Pen pen = new Pen(Color.Black, 1))
+                {
+                    Graphics graphics3 = form1.CreateGraphics();
+                    graphics3.DrawPath(pen, myPath);
+                }
+            }
+        }
+        public override void HideDrawingBackGround()
+        {
+            using (GraphicsPath myPath = new GraphicsPath())
+            {
+                myPath.AddLines(new[]
+                {
+                    new Point((int)this.x,  (int)this.y + ((int)this.verDiagLen  / 2)),
+                    new Point((int)this.x + ((int)this.horDiagLen/ 2), (int)this.y),
+                    new Point((int)this.x + (int)this.horDiagLen, (int)this.y + ((int)this.verDiagLen / 2)),
+                    new Point((int)this.x + ((int)this.horDiagLen / 2), (int)this.y + (int)this.verDiagLen ),
+                    new Point((int)this.x, (int)this.y + ((int)this.verDiagLen  / 2))
+                });
+                Graphics graphics3 = form1.CreateGraphics();
+                graphics3.DrawPath(new Pen(form1.BackColor), myPath);
+                
+            }
+        }
+    }
+}
